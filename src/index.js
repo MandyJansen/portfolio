8b65a6af7f9cc6af2610c2d4425fc97f7ce7f483
Mandy Jansen <mandyjansen87@gmail.com>
2020-04-20 15:00:23 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import AOS from 'aos';
+/* initialize Animate on Screen */
+function initAos() {
+    AOS.init();
+}
+
+/* start AOS when page has loaded */
+window.onload = initAos;
+import particles from 'particles.js';
+import Scripts from './js/scripts.js';
+import Particles from './js/particles.js';
+import './sass/styles.scss';
+
+
---
+++ src/js/scripts.js
@@ -0,0 +1,21 @@
+// Letter animation
+/* declare the letters */
+var elems = document.getElementsByClassName("letter");
+
+/*loop trough all the letters */
+Array.from(elems).forEach(v => v.addEventListener('mouseover', function() {
+	
+	/* add hover class only to the one that's hovered */
+	this.classList.add('hover');
+	  
+	/*when cursor leaves the letter, remove the hover class after 1.5 seconds*/
+      this.addEventListener("mouseout", function() {
+		
+		/* variable to access 'this' in setTimeout function */
+		var x = this;
+		
+        setTimeout(function(){ 
+          x.classList.remove('hover');
+        }, 1500);
+     })
+}));
