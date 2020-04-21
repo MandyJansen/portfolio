1d792d8960ff35ac884e9f203cbcd5de95785b8a
Mandy Jansen <mandyjansen87@gmail.com>
2020-04-21 11:55:39 +0200

    Fix anmation on link hover and remove particles.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import AOS from 'aos';
 import initAOS from './js/aos.js';
-import particlesJS from 'particles.js';
-import Particles from './js/particles.js';
 import Scripts from './js/scripts.js';
 import './sass/styles.scss';
 
