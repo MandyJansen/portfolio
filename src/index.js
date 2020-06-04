649f3c254ac6359cbebdb301d67c4788f2203c2c
Mandy Jansen <mandyjansen87@gmail.com>
2020-06-04 08:54:26 +0200

    Init commit.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './sass/main.scss';
 import Init from './js/init.js';
-import initAOS from './js/init.js';
-import Letters from './js/letters.js';
-import Slidemenu from './js/slidemenu.js';
 
 
 
